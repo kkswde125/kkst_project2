2f1e1aa0b2313ead3a0bcc25462a305b291ba6ed
Owner <Owner@192.168.4.107>
2018-03-09 11:05:20 +0900

    454545

--- kkst_project2/src/main/java/com/pro/kkst/utils/ac_MailUtils.java
+++ kkst_project2/src/main/java/com/pro/kkst/utils/ac_MailUtils.java
@@ -1,8 +1,14 @@
 package com.pro.kkst.utils;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class ac_MailUtils {
 	
@@ -15,6 +21,22 @@ public class ac_MailUtils {
 		
 		props.setProperty("mail.smtp.host",host);
 		Session session = Session.getDefaultInstance(props);
+		MimeMessage msg = new MimeMessage(session);
+
+		try {
+			msg.setFrom(new InternetAddress("aaa@bbb.co.kr", "김유신"));
+			msg.addRecipient(Message.RecipientType.TO, new InternetAddress("kkswde125@naver.com", "김경수"));
+			msg.setSubject("javamail test");
+			msg.setContent("test", "text/plain; charset=utf-8");
+			
+			Transport.send(msg);
+			
+		} catch (UnsupportedEncodingException | MessagingException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 	
 
